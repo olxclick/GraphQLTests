58277fc5187ae693a78ee84953f2dd873c561abe
João Lázaro <71394672+olxclick@users.noreply.github.com>
2024-03-27 13:50:26 +0000

    updates

--- Program.cs
+++ Program.cs
@@ -1,32 +1,50 @@
 using Odyssey.MusicMatcher;
 using SpotifyWeb;
 
+/// <summary>
+/// Creating a web application builder.
+/// </summary>
 var builder = WebApplication.CreateBuilder(args);
 
+/// <summary>
+/// Adding HttpClient for SpotifyService.
+/// Added automatically via nswag
+/// Swagger file was included in the documentation
+/// </summary>
 builder.Services.AddHttpClient<SpotifyService>();
 
+/// <summary>
+/// Adding GraphQL server.
+/// </summary>
 builder.Services.AddGraphQLServer()
       .AddQueryType<Query>()
       .AddMutationType<Mutation>()
       .RegisterService<SpotifyService>();
 
-builder
-    .Services
-    .AddCors(options =>
+/// <summary>
+/// Adding CORS (Cross-Origin Resource Sharing) policy.
+/// </summary>
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
     {
-        options.AddDefaultPolicy(builder =>
-        {
-            builder
-                .WithOrigins("https://studio.apollographql.com")
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
+        builder
+            .WithOrigins("https://studio.apollographql.com")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
     });
+});
 
 var app = builder.Build();
 
+/// <summary>
+/// Enabling CORS.
+/// </summary>
 app.UseCors();
 
+/// <summary>
+/// Mapping GraphQL endpoint.
+/// </summary>
 app.MapGraphQL();
 
 app.Run();
